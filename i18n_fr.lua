74e672e5037a7bc171b1cbfa25cccbf8564c0f82
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:17:32 +0200

    i18n: add missing FR translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,9 @@
 return {
     ["Boxes: %1/%2  Moves: %3  Pushes: %4"] = { fr = "Caisses : %1/%2  Déplacements : %3  Poussées : %4" },
     ["Level %1 complete! Moves: %2"]         = { fr = "Niveau %1 terminé ! Déplacements : %2" },
+    ["Restart"]     = { fr = "Recommencer" },
+    ["Level %1/%2"]         = { fr = "Niveau %1/%2" },
+    ["Solved! Moves: %1  Pushes: %2"]         = { fr = "Résolu ! Déplacements : %1  Poussées : %2" },
+    ["Push all crates onto their targets. You can push but not pull."] = { fr = "Poussez toutes les caisses sur leurs cibles. Vous pouvez pousser mais pas tirer." },
+    ["Sokoban"]     = { fr = "Sokoban" },
 }
