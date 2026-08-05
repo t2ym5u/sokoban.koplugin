0a8ea5141b10732630dbfc6d3239f237839636c6
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:12 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,9 +1,9 @@
 return {
-    ["Boxes: %1/%2  Moves: %3  Pushes: %4"] = { fr = "Caisses : %1/%2  Déplacements : %3  Poussées : %4" },
-    ["Level %1 complete! Moves: %2"]         = { fr = "Niveau %1 terminé ! Déplacements : %2" },
-    ["Restart"]     = { fr = "Recommencer" },
-    ["Level %1/%2"]         = { fr = "Niveau %1/%2" },
-    ["Solved! Moves: %1  Pushes: %2"]         = { fr = "Résolu ! Déplacements : %1  Poussées : %2" },
-    ["Push all crates onto their targets. You can push but not pull."] = { fr = "Poussez toutes les caisses sur leurs cibles. Vous pouvez pousser mais pas tirer." },
-    ["Sokoban"]     = { fr = "Sokoban" },
+    ["Boxes: %1/%2  Moves: %3  Pushes: %4"] = { fr = "Caisses : %1/%2  Déplacements : %3  Poussées : %4", es = "Cajas: %1/%2  Movimientos: %3  Empujes: %4", de = "Kisten: %1/%2  Züge: %3  Schübe: %4" },
+    ["Level %1 complete! Moves: %2"]         = { fr = "Niveau %1 terminé ! Déplacements : %2", es = "¡Nivel %1 completado! Movimientos: %2", de = "Level %1 abgeschlossen! Züge: %2" },
+    ["Restart"]     = { fr = "Recommencer", es = "Reiniciar", de = "Neu starten" },
+    ["Level %1/%2"]         = { fr = "Niveau %1/%2", es = "Nivel %1/%2", de = "Level %1/%2" },
+    ["Solved! Moves: %1  Pushes: %2"]         = { fr = "Résolu ! Déplacements : %1  Poussées : %2", es = "¡Resuelto! Movimientos: %1  Empujes: %2", de = "Gelöst! Züge: %1  Schübe: %2" },
+    ["Push all crates onto their targets. You can push but not pull."] = { fr = "Poussez toutes les caisses sur leurs cibles. Vous pouvez pousser mais pas tirer.", es = "Empuje todas las cajas hasta sus objetivos. Puede empujar pero no tirar.", de = "Schiebe alle Kisten auf ihre Zielfelder. Du kannst nur schieben, nicht ziehen." },
+    ["Sokoban"]     = { fr = "Sokoban", es = "Sokoban", de = "Sokoban" },
 }
